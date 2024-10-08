2c36a4f0fce7f03da8cc7c8f71e534eea2f2d33f
uiuiuiui15 <dr.venitus@gmail.com>
2024-10-08 23:09:29 +0500

    Add project files.

---
+++ Tyuiu.VorobevAD.Sprint0.Task0.V30/Program.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.VorobevAD.Sprint0.Task0.V30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(94);
+        }
+    }
+}
