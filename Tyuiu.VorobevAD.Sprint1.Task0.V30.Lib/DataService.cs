f2fcedc7671eda83d8859a772105e87b0e9103f2
uiuiuiui15 <dr.venitus@gmail.com>
2024-10-08 23:55:35 +0500

    fix

--- Tyuiu.VorobevAD.Sprint1.Task0.V30.Lib/DataService.cs
+++ Tyuiu.VorobevAD.Sprint1.Task0.V30.Lib/DataService.cs
@@ -2,11 +2,16 @@
 
 namespace Tyuiu.VorobevAD.Sprint1.Task0.V30.Lib
 {
-    public class DataService
+    public class DataService : ISprint1Task0V30
     {
         public static int SetCount()
         {
             return (20 * 5 - 4);
+        }
+
+        public double Calculate()
+        {
+            throw new NotImplementedException();
         }
     }
 }
